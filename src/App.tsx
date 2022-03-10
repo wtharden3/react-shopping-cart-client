cc1be426985da1216297b4eae992c4e6d1c0eb23
Whitney Harden <wtharden320@gmail.com>
2022-03-10 04:58:29 -0500

    created getProducts fetch function using fakestoreapi

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,20 @@ import { Drawer, LinearProgress, Grid, Badge } from '@material-ui/core';
 import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 //styles
 import { Wrapper } from './App.style';
+//Types
+export type CartItemType = {
+  id: number;
+  category: string;
+  description: string;
+  image: string;
+  price: number;
+  title: string;
+  amount: number; //to keep track of amount in our cart
+}
+
+const getProducts = async (): Promise<CartItemType> => { //declaring here because we don't have to get this on each render
+  return await (await fetch('https://fakestoreapi.com/products')).json();
+}
 
 const App = () => {
   return (
