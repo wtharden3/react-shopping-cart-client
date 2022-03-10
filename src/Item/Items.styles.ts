47c99bd87ef0303f1d183cef8c57a9bbb2c7da43
Whitney Harden <wtharden320@gmail.com>
2022-03-10 07:59:35 -0500

    Item styling

--- src/Item/Items.styles.ts
+++ src/Item/Items.styles.ts
@@ -1,5 +1,28 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  color: #2b2425;
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  width: 100%;
+  border: 1px solid #1a8edb;
+  border-radius: 20px;
+  height: 100%;
+
+  img {
+    max-height: 250px;
+    object-fit: cover;
+    border-radius: 20px 20px 0 0;
+  }
+
+  div{
+    font-family: Arial, Helvetica, sans-serif;
+    padding: 1rem;
+    height: 100%;
+  }
+
+  button {
+    border-radius: 0 0 20px 20px;
+  }
+
 `
